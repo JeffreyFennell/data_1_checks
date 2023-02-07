858f1dc4afdd513a32518f6bd738e8cfb9e6f348
Jeffrey Fennell <jlfennellcode@gmail.com>
2023-02-06 23:42:37 -0500

    Modified commit

---
+++ knowledge_check_1.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+
+big_cats = ["Lions", "Tigers", "Cheetahs", "Leopards"]
+print(big_cats[2])
+
+big_cats[2]
+
+big_cat_rank = {1: 'Lions', 2: 'Tigers', 3: 'Cheetahs', 4: 'Leopards'}
+print(big_cat_rank[4])
+
+big_cat_rank[5]= "Cougars"
+print(big_cat_rank)
+
+big_cat_rank.pop(5)
+print(big_cat_rank)
+
+big_cat_tuple = ("Lions", "Tigers", "Cheetahs", "Leopards")
+print(big_cat_tuple[2])
+
+big_cat_tuple[2]
